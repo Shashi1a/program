34d887f2f4cdd5d3c5860576318c8e9293c8c1c6
shubha96ngi <54605253+shubha96ngi@users.noreply.github.com>
2021-06-02 17:06:47 +0530

    Update oistprog8.py

--- oistprog8.py
+++ oistprog8.py
@@ -3,6 +3,8 @@
 import numpy as np
 import pandas as pd
 import cv2
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from skimage.segmentation import slic
 from skimage.segmentation import mark_boundaries
