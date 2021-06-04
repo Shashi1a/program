ac39f6cc404067ee296d281f41212456a6423a2e
shubha96ngi <54605253+shubha96ngi@users.noreply.github.com>
2021-06-04 10:57:44 +0530

    Update oistprog7-2g.py

--- oistprog7-2g.py
+++ oistprog7-2g.py
@@ -210,6 +210,7 @@ for j in range(len(gray_imgs)):
         sp_segments_slic = slic(imgs[j]&cv2.cvtColor(mask3, cv2.COLOR_GRAY2BGR), n_segments=50, compactness=10, sigma=1, start_label=1)
         print('*****************')
         sp_segments_ids = np.unique(sp_segments_slic)
+        sp_segments_ids = np.array(sp_segments_ids).astype('int')
         print('unique segments are=', sp_segments_ids)
         print('unique segments are=', np.array(sp_segments_ids).astype('int'))
         print('sp_segments_slic=', sp_segments_slic)
@@ -218,7 +219,7 @@ for j in range(len(gray_imgs)):
         #print('so_pixel_list[',j,']=',sp_pixel_list)
         #print('sp_pixel[','j',']=',sp_pixel)
         l = []
-        for k in range(np.array(sp_segments_ids).astype('int')):
+        for k in range (sp_segments_ids):
             l1 = len(sp_pixel)
             l.append(l)
             sp_pixmax = np.max(l)
