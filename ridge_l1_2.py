8ba29433c977871f24c16611a9bff801eda9e43a
Shubhangi Goyal <shubhangi-goyal@deigo-login2.oist.jp>
2021-06-06 18:03:41 +0900

    ssfjk

--- ridge_l1_2.py
+++ ridge_l1_2.py
@@ -1,3 +1,5 @@
+
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 import pandas as pd
 import joypy
 import numpy as np
@@ -6,7 +8,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 #set some display options
-df2 = pd.read_csv('/flash/TerenzioU/DNA_l1_15_0.6.csv')
+df1 = pd.read_csv('/flash/TerenzioU/DNA_l1_15_0.6.csv')
 #create a color gradent function to be used in the colormap parameter
 def color_gradient(x=0.0, start=(0, 0, 0), stop=(1, 1, 1)):
     r = np.interp(x, [0, 1], [start[0], stop[0]])
@@ -15,9 +17,15 @@ def color_gradient(x=0.0, start=(0, 0, 0), stop=(1, 1, 1)):
     return (r, g, b)
 #show the table
 #print(df2.head(3))
-#plot the figure
-plt.figure()
-fig, axes = joypy.joyplot(df2)
+#plot the figure#3
+#plt.figure()
+plt.figure(figsize=(16,10), dpi= 80)
+##%matplotlib inline
+
+fig, axes = plt.subplots(1,1,1)
+axes[0] = joypy.joyplot(df1)
+#ax = plt.gca()
+#plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
 plt.title('Polaron wave function'
           , fontsize=6
           , color='green')
