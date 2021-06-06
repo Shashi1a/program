d73e02813826228e7f7231427bf6646622e190de
shubha96ngi <54605253+shubha96ngi@users.noreply.github.com>
2021-06-06 23:50:31 +0530

    Update ridge_l1_3.py

--- ridge_l1_3.py
+++ ridge_l1_3.py
@@ -7,9 +7,10 @@ import seaborn as sns
 sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
 # getting the data
 df1 = pd.read_csv('/flash/TerenzioU/DNA_l1_15_0.6.csv')
-df = pd.to_numeric(df1.T)
-print('dtype =', df1.dtypes)
-print(pd.to_numeric(df1.T))
+df = df1.T
+df = df.apply(pd.to_numeric) 
+print('dtype =', df.dtypes)
+print(df)
 # we generate a color palette with Seaborn.color_palette()
 pal = sns.color_palette(palette='coolwarm', n_colors=12)
 # in the sns.FacetGrid class, the 'hue' argument is the one that is the one that will be represented by colors with 'palette'
